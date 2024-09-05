4c0199042ceb7f8607f59a47e94762b932f8ee56
Goossyaa <guzman.gus.02@mail.ru>
2024-09-06 03:02:04 +1000

    Add CodeTodoList

---
+++ Assets/Third-party/Editor/CodeTodoList/Example.cs
@@ -0,0 +1,18 @@
+﻿//You can delete this file as its only purpose is to show you an example on how to use the tool ;)
+
+//TODO : Rate this asset :D
+//TODO : Stay awesome
+
+//AUDIO : trigger explosion sfx
+//AUDIO : Need a loop for the wind sfx
+
+//FIX : Sometimes gives an error here
+//FIX : NullRef if reload scene
+
+//OPTIM : Memory footprint can be reduced here
+//OPTIM : This recursive check is not optimal
+//OPTIM : Clean that, i did it in a rush...
+
+//VISUAL : Trigger cool fire particles
+//VISUAL : Enable the character trail FX
+//VISUAL : Maybe add some sparks on hit ?
